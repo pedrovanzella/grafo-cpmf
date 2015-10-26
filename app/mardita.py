6efb25a989583e8b4465b0a3c19d612022c5947d
Pedro Vanzella <pedro@pedrovanzella.com>
2015-10-26 11:13:47 -0200

    Add stub class

---
+++ app/mardita.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+
+class Mardita:
+    def __init__(self):
+        self.nodes = {}
+        self.edges = {}
+
+    def read_file(self):
+        pass
+
+    def reduce_edges(self):
+        pass
+
+
+if __name__ == "__main__":
+    print("hello")
