9058923a239d367c7184f2b0edcf7ca908d8089e
Pedro Vanzella <pedro@pedrovanzella.com>
2015-11-04 15:44:03 -0200

    Let's use adjecency lists
    This way we can update them dynamically

--- app/mardita.py
+++ app/mardita.py
@@ -5,10 +5,41 @@ import sys
 import pygraphviz as pgv
 
 
+class Graph:
+    def __init__(self):
+        self.nodes = []
+
+    def add_node(self, val):
+        n = Graph.Node(val)
+        self.nodes.append(n)
+        return n
+
+    class Node:
+        def __init__(self, val):
+            self.val = val
+            self.edges = []
+
+        def add_edge(self, to, val):
+            e = Graph.Edge(self, to, val)
+            self.edges.append(e)
+            return e
+
+        def remove_edge(self, edge):
+            self.edges.remove(edge)
+
+    class Edge:
+        def __init__(self, fro, to, val):
+            self.fro = fro
+            self.to = to
+            self.val = val
+
+        def update_val(self, val):
+            self.val += val
+
+
 class Mardita:
     def __init__(self):
-        self.nodes = {}
-        self.edges = {}
+        self.graph = Graph()
 
     def read_file(self, filename):
         with open(filename) as f:
@@ -19,10 +50,10 @@ class Mardita:
                 # split on spaces
                 na, nb, val = l.split(" ")
                 # create nodes
-                self.nodes[na] = True
-                self.nodes[nb] = True
-                # create edges
-                self.edges[na + ',' + nb] = int(val)
+                nan = self.graph.add_node(na)
+                nbn = self.graph.add_node(nb)
+                # create edge
+                nan.add_edge(nbn, val)
 
     def make_graph(self):
         graph = pgv.AGraph(directed=True)
@@ -44,6 +75,8 @@ class Mardita:
             return int(self.edges.pop(u + ',' + v))
 
     def adjecent_nodes(self, a):
+        # Temos que retornar com _next_ e afins
+        # para atualizar a lista sempre
         nodes = []
         for u in self.nodes:
             if self.get_edge(a, u) is not None:
@@ -56,13 +89,17 @@ class Mardita:
             for v in self.adjecent_nodes(u):
                 for a in self.adjecent_nodes(v):
                     # temos que verificar se temos saldo
-                    if self.get_edge(v, a) < self.get_edge(u, v):
+                    if self.get_edge(v, a) <= self.get_edge(u, v):
+                        # remove esta aresta
                         tmp = self.remove_edge(v, a)
-                        # Por que não estou removendo a aresta <3,4> aqui?
                         self.edges[u + ',' + v] -= tmp
                         if self.get_edge(u, a):
+                            # Se a aresta jah existe, aumenta o valor
                             self.edges[u + ',' + a] += tmp
                         else:
+                            # Senao, cria ela
+                            # quando eu crio esta aresta, o iterador nao eh
+                            # alterado
                             self.edges[u + ',' + a] = tmp
 
 
